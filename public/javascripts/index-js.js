037c8ddc92381db2d38117c4d1a46a5e8ce23827
Joe Mulick <joemulick@gmail.com>
2017-10-25 11:33:11 -0700

    data loading onto page correctly.

--- public/javascripts/index-js.js
+++ public/javascripts/index-js.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
 
+	// Catholic News / Prayer Section UI / Functionality
+
 	var currentState = 1;
 	
 	$('.news-prayer-switch').click(function() {
   		
-	if($(this).attr('id') == "prayer-input"){
+	if($(this).attr('id') == "news-input"){
 		if(currentState == 1){
 			console.log('you cant click the 1 button because its already being displayed');
 			return;
 		} else {
 			console.log('You Clicked the Option 1 Button');
 			currentState = 1;
-		}
-		
+		}		
 	} else {
 		if(currentState == 2){
 			console.log('you cant click the 2 button because its already being displayed');
