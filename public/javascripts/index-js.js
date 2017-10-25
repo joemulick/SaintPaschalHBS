d9e425f7279afa2039012f88d3a15f7c2c9b1390
Joe Mulick <joemulick@gmail.com>
2017-10-25 16:57:57 -0700

    need to add prayers in the javascript file so they can pre-compile in variables before the button gets clicked. No need to do any server calling

--- public/javascripts/index-js.js
+++ public/javascripts/index-js.js
@@ -19,7 +19,15 @@ $(document).ready(function() {
 	var newsOutputContainerVar;
 	var prayersOutputContainerVar;
 
-	var tempData = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris";
+	// Here i will hard code in the prayers that are to be rendered on button click
+	var prayersList = [
+   'apostles-creed' : 'I believe in God, the Father Almighty, Creator of heaven and earth; and in Jesus Christ, His only Son, our Lord: Who was conceived by the Holy Spirit, born of the Virgin Mary; suffered under Pontius Pilate, was crucified, died and was buried. He descended into hell; the third day He rose again from the dead; He ascended into heaven, is seated at the right hand of God the Father Almighty; from thence He shall come to judge the living and the dead. I believe in the Holy Spirit, the Holy Catholic Church, the communion of Saints, the forgiveness of sins, the resurrection of the body, and life everlasting. Amen.',
+   'our-father' : 'Our Father, Who art in Heaven, hallowed be Thy name; Thy Kingdom come, Thy will be done on earth as it is in Heaven. Give us this day our daily bread; and forgive us our trespasses as we forgive those who trespass against us; and lead us not into temptation, but deliver us from evil. Amen.',
+   'hail-mary' : 'Hail Mary full of Grace, the Lord is with thee. Blessed are thou among women and blessed is the fruit of thy womb Jesus. Holy Mary Mother of God, pray for us sinners now and at the hour of our death Amen. ',
+   'glory-be' : 'Glory be to the Father, and to the Son, and to the Holy Spirit, as it was in the beginning, is now, and ever shall be, world without end. Amen.',
+   'grace-meals' : 'Bless us, O Lord, and these Thy gifts, which we are about to receive from Thy bounty, through Christ our Lord. Amen.',
+   'act-of-love' : 'O Lord God, I love you above all things and I love my neighbor for your sake because you are the highest, infinite and perfect good, worthy of all my love. In this love I intend to live and die. Amen.'
+	];
 	
 	$('.news-prayer-switch').click(function() {
   		
