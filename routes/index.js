08f404bc47be454f4a91415b49d8c2c86703474b
Joe Mulick <joemulick@gmail.com>
2017-11-01 13:30:06 -0700

    took out the last of the auth0 stuff. going to start incorporating passport.js now

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,6 @@ var router = express.Router();
 var passport = require('passport');
 var Article = require('../models/newsModel.js');
 
-var env = {
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  AUTH0_CALLBACK_URL:
-    process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-};
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,43 +16,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/parish-ministries', function(req, res){
 	res.render('main/parish-ministries');
-});
-
-/* This is for the auth0 login */
-
-router.get('/login', passport.authenticate('auth0', {
-  clientID: env.AUTH0_CLIENT_ID,
-  domain: env.AUTH0_DOMAIN,
-  redirectUri: env.AUTH0_CALLBACK_URL,
-  responseType: 'code',
-  audience: 'https://' + env.AUTH0_DOMAIN + '/userinfo',
-  scope: 'openid profile'}),
-  function(req, res) {
-    res.redirect("/");
-});
-
-router.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
-});
-
-router.get('/callback',
-  passport.authenticate('auth0', {
-    failureRedirect: '/failure'
-  }),
-  function(req, res) {
-    res.redirect(req.session.returnTo || '/user');
-  }
-);
-
-router.get('/failure', function(req, res) {
-  var error = req.flash("error");
-  var error_description = req.flash("error_description");
-  req.logout();
-  res.render('failure', {
-    error: error[0],
-    error_description: error_description[0],
-  });
 });
 
 
